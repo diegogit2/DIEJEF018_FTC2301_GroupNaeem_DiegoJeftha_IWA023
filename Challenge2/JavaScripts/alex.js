4c6d848747125bf958554b7348fe4883a2729f3e
diegogit2 <diegojeftha18@gmail.com>
2023-03-29 14:51:15 +0200

    added index and javascript files

---
+++ Challenge2/JavaScripts/alex.js
@@ -0,0 +1,8 @@
+// alex.js
+
+const firstname = "Alex"
+const surname = "Hunter"
+export const role = "Head of Marketing"
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
